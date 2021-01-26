429844418f888b504beb0e7c64afb218f7387956
Thomas Graviou <t.graviou@yeswehack.com>
2021-01-26 21:08:40 +0100

    Excellent timing

--- bourree.js
+++ bourree.js
@@ -1,13 +1,51 @@
 'use strict';
 
+const INTERVALLE_MINIMUM = 15 * 60 * 1000;
+const INTERVALLE_MAXIMUM = 60 * 60 * 1000;
+
 const danses = [
     "La symphonie",
-    "Le rigaudon"
+    "Le rigaudon",
+    "La clameur",
+    "L'internationale",
+    "La marche",
+    "Le menuet",
+    "La gigue",
+    "Le branle",
+    "Le bransle",
+    "L'ôde",
+    "La polka",
+    "Le chœur",
+    "L'harmonie",
+    "L'hymne",
+    "Le tango",
+    "Le ballet",
+    "L'arabesque",
+    "Le boléro",
+    "Le fest-noz"
 ];
 const travailleurs = [
     "de l'URSSAF",
     "des travailleurs",
-    "des tâcherons"
+    "des tâcherons",
+    "des micro-entrepreneurs",
+    "du genre travailleur",
+    "des cégétistes",
+    "des employés",
+    "des péons",
+    "des villageois",
+    "des villageoises",
+    "des syndiqués",
+    "des macronistes",
+    "des gilets-jaunes",
+    "des agriculteurs",
+    "des commis de cuisine",
+    "des bâtisseurs",
+    "de Lionel Jospin",
+    "des tourneurs-fraiseurs",
+    "des boulangers",
+    "des pizzaïolos",
+    "des enculés"
 ];
 
 function aleatoire(maximum, minimum = 0) {
@@ -20,15 +58,16 @@ function selectionAleatoireTitreAmusant() {
     let travailleurIndex = aleatoire(travailleurs.length);
     
     let titreAmusant = `${danses[danseIndex]} ${travailleurs[travailleurIndex]} - Musique médiévale : Frédéric LAURENT - YouTube`;
-    console.log(titreAmusant);
+    // console.log(titreAmusant);
 
     document.title = titreAmusant;
+    console.log('prout');
     document.getElementsByTagName("h1")[0].children[0].innerText = titreAmusant;
 }
 
 function fonctionnementNormal() {
-    let intervalle = aleatoire(2000, 6000);
-    console.log(intervalle);
+    let intervalle = aleatoire(INTERVALLE_MINIMUM, INTERVALLE_MAXIMUM);
+    // console.log(intervalle);
     setTimeout(selectionAleatoireTitreAmusant, intervalle)
     setTimeout(fonctionnementNormal, intervalle);
 }
